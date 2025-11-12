a421046dab8f0c9ca2310f827e51e7d132c3341a
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2025-11-12 03:37:51 -0300

    Remove obsolete VIX analysis scripts and related files; add BTC and NASDAQ analysis scripts with normalization and visualization.

--- rd/lib/align.R
+++
@@ -1,9 +0,0 @@
-devtools::load_all()
-
-vix <- get_ticker("VIX")[, "adjusted"]
-btc <- get_ticker("CBBTCUSD")
-
-res <- align(vix, btc, names = FALSE, timeframe = "MN1", aggregates = "average", aggregate.end = TRUE)
-res
-
-colnames(res)
