733e1461f550d82d35a9d886e478b24220a13e4f
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2025-11-17 03:29:19 -0300

    Update package installation process: remove 'fets' package and install from local directory.

--- dev/install_packages.R
+++ dev/install_packages.R
@@ -1,7 +1,16 @@
+# ChartSmith
 renv::install("/Users/cassio/spec/chartsmith", prompt = FALSE)
 detach("package:chartsmith", unload = TRUE)
 library("chartsmith")
 
+# Spectrader
 renv::install("/Users/cassio/spec/spectrader", prompt = FALSE)
 detach("package:spectrader", unload = TRUE)
 library("spectrader")
+
+# Financial Engineering Time Series
+renv::remove("fets")
+renv::install(paste0("packages/", dir("packages")[1]))
+
+detach("package:fets", unload = TRUE)
+library("fets")
