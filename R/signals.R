3ae5cf857d597891001ade9b56f64eff699b616a
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2025-11-21 17:58:56 -0300

    Filter signals

--- R/signals.R
+++ R/signals.R
@@ -1,5 +1,6 @@
-filter_signals <- function(df_signals, within_days = 30) {
-  df_signals %>%
+filter_signals <- function(df_signals, within_days = 30, max_per_day = NA) {
+  # Apply within_days filter first (per symbol)
+  df_filtered <- df_signals %>%
     dplyr::group_by(symbol) %>%
     dplyr::arrange(date, .by_group = TRUE) %>%
     dplyr::mutate(
@@ -9,4 +10,14 @@ filter_signals <- function(df_signals, within_days = 30) {
     dplyr::filter(keep) %>%
     dplyr::select(-keep) %>%
     dplyr::ungroup()
+  
+  # Filter exceeding max_per_day trades on the same day
+  if (!is.na(max_per_day)) {
+    df_filtered <- df_filtered %>%
+      dplyr::group_by(date) %>%
+      dplyr::slice_head(n = max_per_day) %>%
+      dplyr::ungroup()
+  }
+  
+  df_filtered
 }
