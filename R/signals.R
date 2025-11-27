035772900e5951845c5a111426ff97d40920e882
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2025-11-27 19:58:18 -0300

    Refactor filter_signals function to improve clarity of within_days condition

--- R/signals.R
+++ R/signals.R
@@ -4,8 +4,11 @@ filter_signals <- function(df_signals, within_days = 30, max_per_day = NA) {
     dplyr::group_by(symbol) %>%
     dplyr::arrange(date, .by_group = TRUE) %>%
     dplyr::mutate(
-      keep = date >= lag(date, default = dplyr::first(date) - within_days) +
-        within_days | dplyr::row_number() == 1
+      keep = dplyr::if_else(
+        dplyr::row_number() == 1,
+        TRUE,
+        date >= dplyr::lag(date) + within_days
+      )
     ) %>%
     dplyr::filter(keep) %>%
     dplyr::select(-keep) %>%
